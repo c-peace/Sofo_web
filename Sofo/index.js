ead7e1049352ce7eac3e163d980f1d1c200cd048
c-peace <85606158+c-peace@users.noreply.github.com>
2023-06-18 06:03:23 +0000

    feat: complete Sofo main page

---
+++ Sofo/index.js
@@ -0,0 +1,15 @@
+function goIntro() {
+    window.scrollTo({top: 0, behavior:'smooth'});
+}
+
+function goGoal() {
+    window.scrollTo({top: 1200, behavior:'smooth'});
+}
+
+function goSolution() {
+    window.scrollTo({top: 2400, behavior:'smooth'});
+}
+
+function goVision() {
+    window.scrollTo({top: 3500, behavior:'smooth'});
+}
